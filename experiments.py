ab1066dc83f52153aaeb85be8e70b3b7fb88a24e
raul ramos <rramosp@unal.edu.co>
2023-02-12 18:39:37 -0500

    allow ctrl-c in experiments

--- experiments.py
+++ experiments.py
@@ -3,9 +3,14 @@ import os
 import numpy as np
 import pandas as pd
 from .lib import data
+from .lib import models_multiclass
 from sklearn.model_selection import ParameterSampler
 import matplotlib.pyplot as plt
 import wandb
+from time import sleep
+
+global saved_model
+saved_model = None
 
 def exp_summary(run_ids, outdir, sort_by=[]):
     '''
@@ -141,29 +146,51 @@ def run_experiment(data_generator_split_method,
     if wproject is not None:
         wandb.run.tags = wandb.run.tags + tuple(wandb_tags)             
     print ("-----", psutil.virtual_memory())
-    pcs.fit(epochs=epochs)
-    pcs.plot_val_sample(10); plt.show()
-    r = pcs.summary_result()
-    csv_path = os.path.join(outdir, pcs.run_id + '.csv')
-    r.to_csv(csv_path)
-    if wproject is not None:
-        config = {}
-        for part in ['train', 'val', 'test']:
-            config[f"mae::{part}"] = r[part]['maeprops_on_chip global']
-            if pcs.produces_pixel_predictions():
-                config[f"f1::{part}"] = r[part]['f1 global']
-                config[f"iou::{part}"] = r[part]['iou global']
-        wandb.config.update(config)
-    params_path = os.path.join(outdir, pcs.run_id + ".params")
-    with open(params_path, 'w') as f:
-        f.write(repr(pcs.get_wandb_config()))
-    pcs.empty_caches()
-    
-    print ("--------")
-    print (r[[i=='loss' or 'global' in i for i in r.index]])
-    print ("--------")
-    print (r[[i!='loss' and 'global' not in i for i in r.index]])
-    print ("--------")
 
-    wandb.finish(quiet=True)
+    interrupted = False    
+    try:
+        pcs.fit(epochs=epochs)
+    except KeyboardInterrupt:
+        print ("-----------------------------------------------------------------------------")
+        print ("keyboard interrupt. saving summary. access model in 'experiments.saved_model'")
+        print ("please wait for summary, or ctrl-c again to interrupt")
+        print ("-----------------------------------------------------------------------------")
+        global saved_model
+        saved_model = pcs
+        interrupted = True
+
+    try:
+        pcs.plot_val_sample(10); plt.show()
+        r = pcs.summary_result()
+        csv_path = os.path.join(outdir, pcs.run_id + '.csv')
+        r.to_csv(csv_path)
+        if wproject is not None:
+            config = {}
+            for part in ['train', 'val', 'test']:
+                config[f"mae::{part}"] = r[part]['maeprops_on_chip global']
+                if pcs.produces_pixel_predictions():
+                    config[f"f1::{part}"] = r[part]['f1 global']
+                    config[f"iou::{part}"] = r[part]['iou global']
+            wandb.config.update(config)
+        params_path = os.path.join(outdir, pcs.run_id + ".params")
+        with open(params_path, 'w') as f:
+            f.write(repr(pcs.get_wandb_config()))
+        pcs.empty_caches()
+        
+        print ("--------")
+        print (r[[i=='loss' or 'global' in i for i in r.index]])
+        print ("--------")
+        print (r[[i!='loss' and 'global' not in i for i in r.index]])
+        print ("--------")
+        wandb.finish(quiet=True)
+
+    except KeyboardInterrupt:
+        interrupted = True
+
+    if interrupted:
+        print ("-----------------------------------------------------------------------")
+        print ("waiting 10secs. ctrl-c again if you want to completely interrupt")
+        print ("-----------------------------------------------------------------------")
+        sleep(10)
+
     return pcs
