ddb82e96a765caeb7eb4995cac95b97db53adfdc
rramosp <rramosp@unal.edu.co>
2023-03-13 09:32:31 -0500

    added confs

--- lib/confs/kqm.py
+++ lib/confs/kqm.py
@@ -1,15 +1,21 @@
 from ..models import kqm
 
-qmp01 = dict(
-            model_class = kqm.QMPatchSegmentation,
-            model_init_args = dict(
-                                patch_size=6,
-                                pred_strides=2,
-                                n_comp=64, 
-                                sigma_ini=0.5,
-                                deep=False
-                            )
-            )                            
+qmp01 = dict(model_class = kqm.QMPatchSegmentation,
+             model_init_args = dict(patch_size=6, pred_strides=6, n_comp=16, sigma_ini=None,deep=False))                            
+
+qmp02 = dict(model_class = kqm.QMPatchSegmentation,
+             model_init_args = dict(patch_size=6, pred_strides=6, n_comp=32, sigma_ini=None,deep=False))                            
+
+qmp03 = dict(model_class = kqm.QMPatchSegmentation,
+             model_init_args = dict(patch_size=6, pred_strides=6, n_comp=64, sigma_ini=None,deep=False))                            
+
+qmp04 = dict(model_class = kqm.QMPatchSegmentation,
+             model_init_args = dict(patch_size=6, pred_strides=6, n_comp=128, sigma_ini=None,deep=False))                            
+
+qmp05 = dict(model_class = kqm.QMPatchSegmentation,
+             model_init_args = dict(patch_size=6, pred_strides=6, n_comp=256, sigma_ini=None,deep=False))                            
+
+
 
 aeqm = dict(
             model_class = kqm.AEQMPatchSegm,
